06d40221f79f19a68a8b9feca63017d4fca36f2d
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2020-11-23 21:04:48 +0000

    perimeter pf circle

---
+++ perimeter_of_circle.cpp
@@ -0,0 +1,16 @@
+// Copyright (C) 2002 Donggeun Lim All rights reserved
+//
+// Created by Donggeun Lim
+// Created on November 2020
+// This program calculates the area and perimeter of a cirle
+// with radius of 15mm
+
+#include <iostream>
+#include <cmath>
+
+int main()  {
+    std::cout << "if\\If a circle has a radius of 15:" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is " << (M_PI*pow(15.0, 2.0)) << "mm²" << std::endl;
+    std::cout << "Perimeter is " << (2*M_PI*15) << "mm" << std::endl;
+    }
